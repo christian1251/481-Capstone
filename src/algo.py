a1f8f80b2ea419b6044c33f029a7cefbd9592013
christian1251 <christianc905@csu.fullerton.edu>
2025-11-19 23:30:54 -0800

    set up algo

--- src/algo.py
+++ src/algo.py
@@ -3,3 +3,15 @@
 #   - IDA* algorithm
 
 # Could make our own or use AIMA from prev hw
+
+class Solver:
+    
+    def __init__(self, cube):
+        self.cube = cube
+        
+    
+    def IDA_STAR():
+        pass
+    
+    def IDDFS():
+        pass
